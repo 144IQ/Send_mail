8af1aeda9e179e26435f3c10a9c4355c6f9dc8b0
遅刻常習犯 <61898931+144IQ@users.noreply.github.com>
2023-04-24 21:38:18 +0900

    Update first.py

--- first.py
+++ first.py
@@ -2,7 +2,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
-EMAIL, PASSWORD = ('example@gmx.us', 'password')
+EMAIL, PASSWORD = ('mail address', 'password')
 CONTENT = "送るメッセージ"
 
 message = MIMEText(CONTENT)
@@ -10,7 +10,7 @@ message["Subject"] = "題名"
 message["From"] = formataddr(("自分の名前", EMAIL))
 message["To"] = '送信先のメールアドレス'
 
-with smtplib.SMTP('smtp.gmx.com', 587) as smtpobj:
+with smtplib.SMTP('server', "Port") as smtpobj:
     smtpobj.starttls()
     smtpobj.login(EMAIL, PASSWORD)
     smtpobj.send_message(message)
